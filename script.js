b2459e2107f666b98330ebd124ed484ed8a38481
Suelen Dantas <suelentd89@gmail.com>
2024-05-28 09:44:50 -0300

    :boom: initial commit

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle('light')
+  html.classList.toggle("light")
 
   //pegar tag
   const img = document.querySelector("#profile img")
 
   //substituir tag
-  if (html.classList.contains('light')){
-    img.setAttribute('src', './assets/suelen1.png')
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/suelen1.png")
   } else {
-    img.setAttribute('src', './assets/suelen1.png')
+    img.setAttribute("src", "./assets/suelen1.png")
   }
-
 
   //se lightmode adicionar nova
 
   //se darkmode manter
-
-
-
 }
-
-
 
 /* trocar imagem com if
 if(html.classList.contains('light')){
